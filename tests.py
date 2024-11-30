f82c0a3c33705bd670fd19a76083dafb1ead5548
-tox-dht-key- <j55694365@gmail.com>
2024-11-30 18:28:04 -0500

    nicer indentation in self-test message
    ''

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@
 def main(args=None):
   if args==None:
     print('''Welcome to tests.py. This is a self-test of the test script,
-written in Python, which in order to be more suitable to rapid tests,
-is typically less efficient than c programming language.''')
+  written in Python, which in order to be more suitable to rapid tests,
+  is typically less efficient than c programming language.''')
 
 if __name__ == '__main__':
     main()
