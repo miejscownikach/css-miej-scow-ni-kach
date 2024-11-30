1e18a6a0db3d4c354e2464fadaebc8ed74cfae81
-tox-dht-key- <j55694365@gmail.com>
2024-11-30 18:36:36 -0500

    main test function to do main tests.
    placeholder to parse command-line arguments, and main test function.

--- tests.py
+++ tests.py
@@ -1,10 +1,24 @@
 '''tests.py'''
+from sys import argv
+
+def default():
+  print('''main test-bed. other tests should be moved off to other test functions,
+  after they are satisfactory.''')
+
+  print('example test: print function works(probably)')
+
+def parse_args(functions=['default']): # placeholder
+  return functions
 
 def main(args=None):
   if args==None:
     print('''Welcome to tests.py. This is a self-test of the test script,
   written in Python, which in order to be more suitable to rapid tests,
   is typically less efficient than c programming language.''')
+  
+  functions = parse_args(args)
+  for function in functions:
+    function()
 
 if __name__ == '__main__':
-    main()
+    main(argv)
