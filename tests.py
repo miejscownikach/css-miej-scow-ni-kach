0e6c634d6f341ec854150447946001ab3ea0a0a2
-tox-dht-key- <j55694365@gmail.com>
2024-11-30 18:24:58 -0500

    run the main function
    running the main function allows running the file as an entry point

--- tests.py
+++ tests.py
@@ -5,3 +5,6 @@ def main(args=None):
     print('''Welcome to tests.py. This is a self-test of the test script,
     written in Python, which trades off efficiency, to be easier to test
     and deploy rapid changes in source code.''')
+
+if __name__ == '__main__':
+    main()
