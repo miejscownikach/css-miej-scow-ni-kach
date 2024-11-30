ab11ae5e8188d232f855f7266d1fd0e4803e6a63
-tox-dht-key- <j55694365@gmail.com>
2024-11-30 17:57:40 -0500

    i hope this works.
    tests file to launch other tests.

---
+++ tests.py
@@ -0,0 +1,7 @@
+'''tests.py'''
+
+def main(args=None):
+  if args==None:
+    print('''Welcome to tests.py. This is a self-test of the test script,
+    written in Python, which trades off efficiency, to be easier to test
+    and deploy rapid changes in source code.''')
